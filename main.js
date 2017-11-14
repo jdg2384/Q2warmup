9b952ce6c44f028e8b411b3ee173a3da6a91e989
Jonathan Carson <carsonjd@colorado.edu>
2017-11-14 09:38:15 -0700

    added color change to click event

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var hw = document.getElementsByTagName('h1');
 h1.addEventListener('click', function(event){
-  event.target.css('background-color', 'green');
+  event.target.css({'background-color': 'green', 'color': 'red'});
 });
